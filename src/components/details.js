c752b2e468faced920d81725640b953e8405e4d9
aakbarkhan <aakbarkhan@gmail.com>
2022-01-13 20:39:15 +0530

    Fix the list detail color pattern

--- src/components/details.js
+++ src/components/details.js
@@ -33,118 +33,62 @@ const Details = (props) => {
               <p className="dat-type">TOTAL DEATH:</p>
               <b className="dat-type">{cou.data.today_deaths}</b>
             </div>
-          </div>
-          <div className="sor">
-            <p>
-              CONFIRMED:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_new_confirmed}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              TODAY DEATH:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_new_deaths}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_new_open_cases:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_new_open_cases}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_new_recovered:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_new_recovered}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_open_cases:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_open_cases}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_recovered:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_recovered}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_vs_yesterday_confirmed:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_vs_yesterday_confirmed}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_vs_yesterday_deaths:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_vs_yesterday_deaths}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_vs_yesterday_open_cases:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_vs_yesterday_open_cases}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              today_vs_yesterday_recovered:
-            </p>
-            <b className="dat-type">
-              {cou.data.today_vs_yesterday_recovered}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              yesterday_confirmed:
-            </p>
-            <b className="dat-type">
-              {cou.data.yesterday_confirmed}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              yesterday_deaths:
-            </p>
-            <b className="dat-type">
-              {cou.data.yesterday_deaths}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              yesterday_open_cases:
-            </p>
-            <b className="dat-type">
-              {cou.data.yesterday_open_cases}
-            </b>
-          </div>
-          <div className="sor">
-            <p>
-              yesterday_recovered:
-            </p>
-            <b className="dat-type">
-              {cou.data.yesterday_recovered}
-            </b>
+            <div className="sor">
+              <p className="dat-type">CONFIRMED:</p>
+              <b className="dat-type">{cou.data.today_new_confirmed}</b>
+            </div>
+            <div className="sor">
+              <p>TODAY DEATH:</p>
+              <b className="dat-type">{cou.data.today_new_deaths}</b>
+            </div>
+            <div className="sor">
+              <p>today_new_open_cases:</p>
+              <b className="dat-type">{cou.data.today_new_open_cases}</b>
+            </div>
+            <div className="sor">
+              <p>today_new_recovered:</p>
+              <b className="dat-type">{cou.data.today_new_recovered}</b>
+            </div>
+            <div className="sor">
+              <p>today_open_cases:</p>
+              <b className="dat-type">{cou.data.today_open_cases}</b>
+            </div>
+            <div className="sor">
+              <p>today_recovered:</p>
+              <b className="dat-type">{cou.data.today_recovered}</b>
+            </div>
+            <div className="sor">
+              <p>today_vs_yesterday_confirmed:</p>
+              <b className="dat-type">{cou.data.today_vs_yesterday_confirmed}</b>
+            </div>
+            <div className="sor">
+              <p>today_vs_yesterday_deaths:</p>
+              <b className="dat-type">{cou.data.today_vs_yesterday_deaths}</b>
+            </div>
+            <div className="sor">
+              <p>today_vs_yesterday_open_cases:</p>
+              <b className="dat-type">{cou.data.today_vs_yesterday_open_cases}</b>
+            </div>
+            <div className="sor">
+              <p>today_vs_yesterday_recovered:</p>
+              <b className="dat-type">{cou.data.today_vs_yesterday_recovered}</b>
+            </div>
+            <div className="sor">
+              <p>yesterday_confirmed:</p>
+              <b className="dat-type">{cou.data.yesterday_confirmed}</b>
+            </div>
+            <div className="sor">
+              <p>yesterday_deaths:</p>
+              <b className="dat-type">{cou.data.yesterday_deaths}</b>
+            </div>
+            <div className="sor">
+              <p>yesterday_open_cases:</p>
+              <b className="dat-type">{cou.data.yesterday_open_cases}</b>
+            </div>
+            <div className="sor">
+              <p>yesterday_recovered:</p>
+              <b className="dat-type">{cou.data.yesterday_recovered}</b>
+            </div>
           </div>
         </div>
       </div>
