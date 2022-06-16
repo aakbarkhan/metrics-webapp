b3e9b4ab92e8d2ca5669734e125474213abd3c3f
aakbarkhan <aakbarkhan@gmail.com>
2022-06-17 00:53:46 +0530

    Update the new api and all the datastructure

--- src/components/detailList.js
+++ src/components/detailList.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getCovidFromApi, getCountry } from '../redux/actions/fetchapi';
+// import { useParams } from 'react-router-dom';
+import { getCovidFromApi } from '../redux/actions/fetchapi';
 import Details from './details';
 
 const DetailsList = () => {
@@ -12,22 +12,22 @@ const DetailsList = () => {
       dispatch(getCovidFromApi());
     }
   }, []);
-  const todayDate = new Date().toISOString().slice(0, 10);
-  const handleCountry = () => {
-    const country = useParams();
-    const hello = country.country;
-    // const num = document.getElementById('date').value;
-    const baseUrldate = `https://api.covid19tracking.narrativa.com/api/${todayDate}/country/${hello}`;
-    const api = () => async () => {
-      const request = await fetch(baseUrldate);
-      const response = await request.json();
-      const data = response.dates[todayDate].countries[hello];
-      dispatch(getCountry(data));
-    };
-    dispatch(api());
-  };
+  // const todayDate = new Date().toISOString().slice(0, 10);
+  // const handleCountry = () => {
+  //   const country = useParams();
+  //   const hello = country.country;
+  //   // const num = document.getElementById('date').value;
+  //   const baseUrldate = `https://api.covid19tracking.narrativa.com/api/${todayDate}/country/${hello}`;
+  //   const api = () => async () => {
+  //     const request = await fetch(baseUrldate);
+  //     const response = await request.json();
+  //     const data = response.dates[todayDate].countries[hello];
+  //     dispatch(getCountry(data));
+  //   };
+  //   dispatch(api());
+  // };
   if (dataCovid.country.length === 0) {
-    handleCountry();
+    // handleCountry();
   }
   return (
     <div>
